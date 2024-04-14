a2a5d470e11d1bc921c45283501e24497e4561d7
Raviteja B <ravitejab2209@gmail.com>
2024-04-15 00:12:40 +0530

    handling request changes

--- app.js
+++ app.js
@@ -97,6 +97,11 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get('/', (req, res, next) => {
+    res.redirect('/listings');
+    next();
+});
+
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found! :("))
