cb9c9f4ec0d8ea1f6e893bad48b1d5e312692ae5
Raviteja B <ravitejab2209@gmail.com>
2024-04-15 18:49:20 +0530

    minor change in logic

--- app.js
+++ app.js
@@ -97,9 +97,13 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 
 app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Click on the Red Icon/ Explore!"))
+    next(new ExpressError(404, "Page not found!"))
 })
 
 app.use((err, req, res, next) => {
